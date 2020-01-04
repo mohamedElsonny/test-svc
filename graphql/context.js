eafd3176fc8472dd48d2761bf4386068627b1224
Mohamed Gamal <mohamedelsonny5@gmail.com>
2020-01-04 15:22:39 +0200

    added secret

--- graphql/context.js
+++ graphql/context.js
@@ -1,9 +1,11 @@
 const { Prisma } = require('prisma-binding')
 const path = require('path')
+
 const prisma = new Prisma({
   endpoint: `${process.env.PRISMA_ENDPOINT}/${process.env.PRISMA_DB}/${process.env.PRISMA_STAGE}`,
   typeDefs: path.resolve(__dirname, '../generated/prisma.graphql'),
-  debug: false
+  debug: false,
+  secret: process.env.PRISMA_SECRET
 })
 
 module.exports = { prisma }
